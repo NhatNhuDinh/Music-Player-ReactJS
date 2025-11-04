407cfd33eb9228373e602ae65f96cec463c0e24d
NhatNhu <nhudinhnhat2004@gmail.com>
2025-11-04 10:59:38 +0700

    ok

--- src/assets/js/songs.js
+++ src/assets/js/songs.js
@@ -3,38 +3,38 @@ const songs = [
     {
         name: 'Cho Tôi Đi Theo',
         singer: 'Ngọt',
-        path: '/src/assets/music/song1.mp3',
-        image: '/src/assets/img/song1.jpg'
+        path: '/public/music/song1.mp3',
+        image: '/public/img/song1.jpg'
     },
     {
         name: 'THE PLAYAH',
         singer: 'Soobin Hoàng Sơn',
-        path: '/src/assets/music/song6.mp3',
-        image: '/src/assets/img/song6.jpg'
+        path: '/public/music/song6.mp3',
+        image: '/public/img/song6.jpg'
     },
     {
         name: 'Chiều Hôm Ấy',
         singer: 'JayKii',
-        path: '/src/assets/music/song2.mp3',
-        image: '/src/assets/img/song2.jpg'
+        path: '/public/music/song2.mp3',
+        image: '/public/img/song2.jpg'
     },
     {
         name: 'dự báo thời tiết hôm nay mưa',
         singer: 'Grey D',
-        path: '/src/assets/music/song3.mp3',
-        image: '/src/assets/img/song3.jpg'
+        path: '/public/music/song3.mp3',
+        image: '/public/img/song3.jpg'
     },
     {
         name: 'Gác Lại Âu Lo',
         singer: 'Da LAB ft. Miu Lê',
-        path: '/src/assets/music/song4.mp3',
-        image: '/src/assets/img/song4.jpg'
+        path: '/public/music/song4.mp3',
+        image: '/public/img/song4.jpg'
     },
     {
         name: 'Hôm Nay Tôi Buồn',
         singer: 'Phùng Khánh Linh',
-        path: '/src/assets/music/song5.mp3',
-        image: '/src/assets/img/song5.jpg'
+        path: '/public/music/song5.mp3',
+        image: '/public/img/song5.jpg'
     },
 ]
 
