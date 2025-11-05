797c73202a595f20403e48296ee55ba692574641
NhatNhu <nhudinhnhat2004@gmail.com>
2025-11-05 09:36:44 +0700

    update path

--- src/assets/js/songs.js
+++ src/assets/js/songs.js
@@ -1,42 +1,25 @@
+// src/assets/js/songs.js
+import song1 from '../music/song1.mp3'
+import song2 from '../music/song2.mp3'
+import song3 from '../music/song3.mp3'
+import song4 from '../music/song4.mp3'
+import song5 from '../music/song5.mp3'
+import song6 from '../music/song6.mp3'
+
+import img1 from '../img/song1.jpg'
+import img2 from '../img/song2.jpg'
+import img3 from '../img/song3.jpg'
+import img4 from '../img/song4.jpg'
+import img5 from '../img/song5.jpg'
+import img6 from '../img/song6.jpg'
 
 const songs = [
-    {
-        name: 'Cho Tôi Đi Theo',
-        singer: 'Ngọt',
-        path: '/public/music/song1.mp3',
-        image: '/public/img/song1.jpg'
-    },
-    {
-        name: 'THE PLAYAH',
-        singer: 'Soobin Hoàng Sơn',
-        path: '/public/music/song6.mp3',
-        image: '/public/img/song6.jpg'
-    },
-    {
-        name: 'Chiều Hôm Ấy',
-        singer: 'JayKii',
-        path: '/public/music/song2.mp3',
-        image: '/public/img/song2.jpg'
-    },
-    {
-        name: 'dự báo thời tiết hôm nay mưa',
-        singer: 'Grey D',
-        path: '/public/music/song3.mp3',
-        image: '/public/img/song3.jpg'
-    },
-    {
-        name: 'Gác Lại Âu Lo',
-        singer: 'Da LAB ft. Miu Lê',
-        path: '/public/music/song4.mp3',
-        image: '/public/img/song4.jpg'
-    },
-    {
-        name: 'Hôm Nay Tôi Buồn',
-        singer: 'Phùng Khánh Linh',
-        path: '/public/music/song5.mp3',
-        image: '/public/img/song5.jpg'
-    },
+  { name: 'Cho Tôi Đi Theo', singer: 'Ngọt',               path: song1, image: img1 },
+  { name: 'THE PLAYAH',     singer: 'Soobin Hoàng Sơn',    path: song6, image: img6 },
+  { name: 'Chiều Hôm Ấy',   singer: 'JayKii',              path: song2, image: img2 },
+  { name: 'dự báo thời tiết hôm nay mưa', singer: 'Grey D', path: song3, image: img3 },
+  { name: 'Gác Lại Âu Lo',  singer: 'Da LAB ft. Miu Lê',   path: song4, image: img4 },
+  { name: 'Hôm Nay Tôi Buồn', singer: 'Phùng Khánh Linh',  path: song5, image: img5 },
 ]
 
-
-export default songs;
+export default songs
--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
-export default { base: '/Music-Player-ReactJS/' }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Music-Player-ReactJS/'
+})
